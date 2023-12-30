f34ce0fced411f992a495c90a8730d5a1a60a6a0
halim13 <hasanudinhalim@gmail.com>
2023-12-30 18:54:44 +0700

    first blood

---
+++ README.md
@@ -0,0 +1,9 @@
+# googlenavbar
+
+A new Flutter project.
+
+## Thanks To
+
+[Mitch Koko](https://www.youtube.com/watch?v=FEvYl8Mzsxw)
+
+## Result
---
+++ android/app/src/main/kotlin/com/example/googlenavbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.googlenavbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
